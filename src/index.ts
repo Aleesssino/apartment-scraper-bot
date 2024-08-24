3c94c4e8caa221883bc5be3526da7e4a72006a8e
Aleesssino <alesg3a1b4o1@gmail.com>
2024-08-24 05:53:17 +0200

    chore: Inicialize Telegram bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 console.log("give me some sweet scrapes");
+
+import "dotenv/config";
+import TelegramBot from "node-telegram-bot-api";
+
+const token = process.env.TELEGRAM_TOKEN as string;
+const chatId = process.env.TELEGRAM_CHAT_ID as string;
+
+const bot = new TelegramBot(token);
+
+const sendMessage = async (message: string) => {
+  try {
+    await bot.sendMessage(chatId, message);
+    console.log("Message sent successfully");
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
+};
+
+sendMessage("We rollin!");
