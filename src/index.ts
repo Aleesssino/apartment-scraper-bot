50830edb3d13fdd29b2fea209c19583635e3fcbb
Aleesssino <alesg3a1b4o1@gmail.com>
2024-08-26 12:39:53 +0200

    fix: Stop sending 1+kk, 2+kk.. offers

--- src/index.ts
+++ src/index.ts
@@ -159,7 +159,7 @@ const writeJsonFile = async (
 //
 const main = async () => {
   const sreality =
-    "https://www.sreality.cz/hledani/pronajem/byty/brno?velikost=3%2Bkk,4%2Bkk,4%2B1,5%2Bkk,3%2B1,5%2B1&stari=mesic&cena-od=0&cena-do=40000";
+    "https://www.sreality.cz/hledani/pronajem/byty/brno?velikost=3%2Bkk,4%2Bkk,4%2B1,5%2Bkk,3%2B1,5%2B1&stari=tyden&plocha-od=70&plocha-do=10000000000&cena-od=0&cena-do=40000";
   try {
     const browser: Browser = await puppeteer.launch({
       headless: true,
